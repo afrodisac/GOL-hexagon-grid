6b6e29280685ae9322399376199b11c122936ea7
afrodisiac <eric.gericke2@gmail.com>
2022-03-08 14:27:03 +0100

    correct rules!!!! and correct neighbour counting!!

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import random
 import numpy as np
 from math import cos, sin, pi, tan
 
+
+
 width, height = 1000, 1000
 size = (width, height)
 bgcolour = (0, 0, 0)
-ON_COLOUR = (0, 0, 0)
-OFF_COLOUR = (255, 255, 255)
+ON_COLOUR = (106,13, 173)
+OFF_COLOUR = (60, 60, 60)
 radius = 25
 fps = 10
-columns = round((width/2))//radius
-rows = (round(height/1.5))//radius
+columns = width//radius
+rows = height//radius
 grid_size =(columns, rows)
 apothem = radius / (2*tan(pi/6))
 def main():
@@ -27,17 +29,17 @@ def main():
         game_of_life(surface, array)
         array = next_array(array, pause)
         clock.tick(fps)
+        # pygame.time.delay(500)
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 run = False
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_p:
                     pause = not pause
-            if event.type ==pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
                 x,y = pygame.mouse.get_pos()
                 x= x//radius%rows
                 y= y//radius%columns
-                print(x,y)
                 array[x][y] = 1
 
         pygame.display.update()
@@ -67,23 +69,58 @@ def next_array(array, pause):
             for y in range(rows):
                 state = next[x][y]
                 neighbours = get_neighbours(array, x, y)
-                if state == 1 and neighbours < 2 or neighbours >3:
+                if state == 1 and neighbours < 2 or neighbours >2:
                     next[x][y] = 0
-                elif state == 0 and neighbours == 3:
+                    # print(x,y)
+                elif state == 0 and neighbours == 2:
                     next[x][y] = 1
+                    print(x,y)
         array = next
         return array  
     else:
         return array    
 
 
-def get_neighbours(next, x, y):
+# def get_neighbours(next, x, y):
+#     total = 0
+#     for i in range(-1, 1):
+#         for j in range(-1, 1):
+#             x_edge = (x + j + columns)%columns
+#             y_edge = (y + i + rows)%rows
+#             total += next[x_edge][y_edge]
+#     total -= next[x][y]
+#     return total
+
+# def get_neighbours(next, x, y):
+#     total = 0
+#     total += next[(x+1 + columns)%columns][(y -1 + rows)%rows]
+#     total += next[(x +1+ columns)%columns][(y + rows)%rows]
+#     total += next[(x + columns)%columns][(y+1 +rows)%rows]
+#     total += next[(x -1 + columns)%columns][(y +1 + rows)%rows]
+#     total += next[(x-1 + columns)%columns][(y + rows)%rows]
+#     total += next[(x + columns)%columns][(y -1 + rows)%rows]
+    
+#     total -= next[x][y]
+#     return total
+
+def get_neighbours(next, x,y):
     total = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            x_edge = (x + i + columns)%columns
-            y_edge = (y + j + rows)%rows
-            total += next[x_edge][y_edge]
+    even_row = [[+1, 0], [0, -1], [-1, -1], [-1, 0], [-1, +1], [0, +1]]
+    odd_row = [[+1,  0], [+1, -1], [ 0, -1], [-1,  0], [ 0, +1], [+1, +1]]
+    if y %2 == 0:
+        total += next[(x +even_row[0][0] + columns)%columns][(y+ even_row[0][1] +rows)%rows]
+        total += next[(x +even_row[1][0] + columns)%columns][(y+ even_row[1][1] +rows)%rows]
+        total += next[(x +even_row[2][0] + columns)%columns][(y+ even_row[2][1] +rows)%rows]
+        total += next[(x +even_row[3][0] + columns)%columns][(y+ even_row[3][1] +rows)%rows]
+        total += next[(x +even_row[4][0] + columns)%columns][(y+ even_row[4][1] +rows)%rows]
+        total += next[(x +even_row[5][0] + columns)%columns][(y+ even_row[5][1] +rows)%rows]
+    else:
+        total += next[(x +odd_row[0][0] + columns)%columns][(y+ odd_row[0][1] +rows)%rows]
+        total += next[(x +odd_row[1][0] + columns)%columns][(y+ odd_row[1][1] +rows)%rows]
+        total += next[(x +odd_row[2][0] + columns)%columns][(y+ odd_row[2][1] +rows)%rows]
+        total += next[(x +odd_row[3][0] + columns)%columns][(y+ odd_row[3][1] +rows)%rows]
+        total += next[(x +odd_row[4][0] + columns)%columns][(y+ odd_row[4][1] +rows)%rows]
+        total += next[(x +odd_row[5][0] + columns)%columns][(y+ odd_row[5][1] +rows)%rows]
     total -= next[x][y]
     return total
 
@@ -91,7 +128,8 @@ def make_random_grid():
     array = np.ndarray(shape=(grid_size))
     for i in range(columns):
         for j in range(rows):
-            array[i][j] = random.randint(0,1)
+            array[i][j] = 0# random.randint(0,1)
+
     return array
 
 if __name__ == '__main__':
